42500f2b798c45ceec07833fee2c5f2d4f63f5e7
Jennifer <jjtishrean@aol.com>
2025-01-19 10:31:13 -0800

    experiment: storing fractions in the matrix as tuples

--- scale.py
+++ scale.py
@@ -1,4 +1,9 @@
 #moving the scaling funtion to its own folder so I can attempt to add the fraction reduction feature
+#Question: can I represent fractions in the matrix as tuples? Tuples can't be changed, but I can overwrite them, right?
+
+#Answer: this seems to work. However, now I'v introduced a pretty large issue in that not all the values in my 
+#matrix are actually numbers that can have math done on them. Is there a better solution? I don't want, for example,
+#1/3 to become 0.33, because that will cause problems later.
 from math import gcd
 
 test_matrix = [[2,4,6],[0,-7,-4],[3,1,-1]]
@@ -9,10 +14,17 @@ def reduce_frac (x,y):
     y = y // g_c_d
     return ((x,y))
 
-def row_scale (my_matrix: list, row1: int, multiplier: float) -> list:
+def row_scale (my_matrix: list, row1: int, divider: float) -> list:
     for i in range (len(my_matrix[row1-1])):
-        my_matrix[row1-1][i] =  my_matrix[row1-1][i]* multiplier
+        temp_element = reduce_frac(my_matrix[row1-1][i],divider)
+        if temp_element[1] == 1:
+            my_matrix[row1-1][i] = temp_element[0]
+        else:
+            my_matrix[row1-1][i] = temp_element
     return (my_matrix)
 
-scaled_matrix = row_scale (test_matrix,1,0.5)
+scaled_matrix = row_scale (test_matrix,2,2)
 print (scaled_matrix)
+
+# test_frac = reduce_frac(2,4)
+# print(type(test_frac))
