06478088adf915bfb57709f4779068e4b7b81fcb
caarrp <junebirdsfly@gmail.com>
2025-06-13 22:09:12 -0700

    new naming is this restful though?

--- web_app.py
+++ web_app.py
@@ -54,41 +54,12 @@ class rref_helper:
             return flask.render_template("index.html"), 200
 
 
-        @self.app.route("/<path:arg>")
-        def main(arg):
-            """
-            Handle all file requests and return appropriate responses.
-
-            Args:
-                arg: URL path
-            Returns:
-                HTML file if it exists, otherwise appropriate error page, when
-                for example,
-                `return render_template("404.html"), 404` when the requested page does not exist.
-            """
-            FORBIDDEN_SYMBOLS = ["//", "..", "~"]
-
-            for symbol in FORBIDDEN_SYMBOLS:
-                if symbol in arg:
-                    self.app.logger.debug("made it to forbidden 403")
-                    return flask.render_template("403.html"), 403
-
-            if not arg.endswith(".html"):
-                self.app.logger.debug("made it to not html 405")
-                return flask.render_template("405.html"), 405
-
-            if arg == "index.html":
-                return flask.render_template("index.html")
-
-            try:
-                fp = os.path.join("templates", arg)
-                self.app.logger.debug("made it!")
-                return flask.render_template("/"), 200
-            
-            except:
-                self.app.logger.debug("made it to not found 404")
-                return flask.render_template("404.html"), 404
-    
+        @self.app.route("/enter_matrix")
+        def enter_matrix(arg):
+            """ somehow ajax the entry of rows cols and
+            dynamically create html or js rows cols to enter values"""
+            pass
+        
     def run(self):
         self.app.logger.info("Starting web server on port %d", self.port)
         self.app.run(port=self.port, host="0.0.0.0", debug=self.app.debug)
