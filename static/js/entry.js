f67b45769f95e81fc52caebab30d8bfa0b4be539
caarrp <junebirdsfly@gmail.com>
2025-06-13 22:03:06 -0700

    other random edts im tired.

---
+++ static/js/entry.js
@@ -0,0 +1,35 @@
+
+
+
+
+// Suppress normal form submission
+$("#entry").submit( function(event) {
+   event.preventDefault();
+  });
+
+// Keep track of the current AJAX request
+var currentRequest = null;
+
+// Function to validate and update input
+function validateInput(inputId) {
+    var input = $(`#${inputId}`).val();
+    var num = input.replace(/[^0-9]/g, '');
+    
+    if (parseInt(num, 10) > 25) {
+        num = "24";
+    }
+    
+    $(`#${inputId}`).val(num);
+    return num;
+}
+
+// Function to reset the display
+function resetDisplay() {
+    $("#rows").val("");
+    $("#cols").val("");
+}
+
+$("#reset").click(function() {
+    resetDisplay();
+});
+
