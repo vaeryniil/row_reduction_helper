a8290c3b732d367dba7e4556fb134f542fa99b0c
Jennifer <jjtishrean@aol.com>
2025-08-05 11:17:56 -0700

    complete Matrix.js

--- Matrix_old.js
+++ Matrix_old.js
@@ -25,14 +25,6 @@ function reduce_frac (x,y) { // x is numerator, y is denominator
     
     return [x,y] 
 }
-
-//a helper function to multiply (and reduce) fractions
-function fraction_multiplier(no_1,no_2) {///multiplies two arrays with numerator in position 0 and denominator in pos. 1
-    const numerator = no_1[0] * no_2[0];
-    const denominator = no_1[1] * no_2[1];
-    let solution = reduce_frac(numerator, denominator);
-    return solution
-}
  
  
  /* A matrix has a size mxn, state (which version/step it is in the reduction process) and entries represented as arrays of length 2, 
@@ -105,47 +97,11 @@ class Matrix {
         return this.entries;
     }
 
-
-    //note: multiplier parameter must be passed in as an array. Ex: 1/2 is entered as [1,2]; -8 is entered as [-8,1]
-    // change row is entered as an int, as is scale_row
-    add(change_row, scale_row, multiplier) {
-
-        let row_len = this.entries[change_row-1].length;
-        for (let i = 0; i < row_len; i++) { 
-    
-            let temp_tuple = fraction_multiplier(this.entries[scale_row-1][i],multiplier); //this scales the appropriate row before adding
-            //console.log(temp_tuple); // scaffolding
-            //console.log(this.entries[scale_row - 1][i]); //scaffolding
-
-            //if the denominators of the two arrays are the same, add the arrays, reduce,  and edit the matrix
-            if (temp_tuple[1] == this.entries[change_row - 1][i][1]) {
-                //console.log("same"); //scaffolding
-                this.entries[change_row-1][i] = reduce_frac(temp_tuple[0] + this.entries[change_row - 1][i][0],this.entries[change_row - 1][i][1]);
-            }
-            
-        else { //need to have a common denominator in order to add 
-            //if the denominators of the two are different, cross-multiply and reduce
-
-            //console.log('different'); //scaffolding
-            let large_den_temp1 = [this.entries[change_row - 1][i][0] * temp_tuple[1],this.entries[change_row - 1][i][1] * temp_tuple[1]];
-            //console.log(large_den_temp1); //scaffolding
-            let large_den_temp2 = [temp_tuple[0] * this.entries[change_row - 1][i][1],temp_tuple[1] * this.entries[change_row - 1][i][1]];
-            //console.log(large_den_temp2); //scaffolding
-            let new_tuple_temp = reduce_frac(large_den_temp1[0] + large_den_temp2[0],large_den_temp1[1]);
-            //console.log(new_tuple_temp); // scaffolding
-            this.entries[change_row-1][i] = new_tuple_temp;
-        }
-    }
-
-    return this.entries;
-}
-
 }
 
 
 
 //let test_matrix = new Matrix ([3,2], [0,1], [[[2,1],[4,1]],[[0,1],[-7,1]],[[3,1],[1,2]]])
-//let test_matrix2 = new Matrix ([3,2], [0,1], [[[2,1],[4,1]],[[0,1],[-7,1]],[[3,1],[-15,2]]])
 
 //console.log(test_matrix)
 
@@ -164,30 +120,3 @@ class Matrix {
 //console.log(test_matrix.scale(3,0,[2,1]))
    
 //testing add
-//fraction test with different denominators
-//let fraction_test = new Matrix([4,5],[0,1],[[[1,1],[2,1],[3,1],[0,1],[4,1]],[[0,1],[3,1],[-7,1],[1,1],[-9,1]],[[0,1],[0,1],[6,1],[0,1],[31,3]],[[0,1],[0,1],[-6,1],[0,1],[-15,2]]]);
-
-
-let fraction_test2 = new Matrix ([2,2], [0,1], [[[1,1],[31,3]],[[0,1],[-15,2]]]);
-console.log(fraction_test2.add(1,2,[62,45])); //should return [ [ [ 1, 1 ], [ 0, 1 ] ], [ [ 0, 1 ], [ -15, 2 ] ] ]
-//fraction test 2 works
-
-/*
-//this is a matrix from a homework assignment. 'add' method works on this. Note: no fractions.
-//final matrix is [[[1,1],[0,1],[0,1]],[[0,1],[1,1],[0,1]],[[0,1],[0,1],[1,1]]]
-let hw_test_m = new Matrix ([3,3],[0,1],[[[1,1],[0,1],[2,1]],[[0,1],[1,1],[3,1]],[[-2,1],[2,1],[3,1]]]);
-console.log("Original matrix:");
-console.log(hw_test_m.entries);
-let step1 = hw_test_m.add(3,1,[2,1]);
-console.log("Step 1");
-console.log(step1);
-let step2 = hw_test_m.add(3,2,[-2,1]);
-console.log("Step 2");
-console.log(step2);
-let step3 = hw_test_m.add(2,3,[-3,1]);
-console.log("Step 3");
-console.log(step3);
-let step4 = hw_test_m.add(1,3,[-2,1]);
-console.log("Row reduced:");
-console.log(step4);
-*/
